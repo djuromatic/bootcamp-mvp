b3f65a7e0833fb4b3d6c74460fde665c94634e8f
Djuro Matic <djuromatic@Djuros-MacBook-Pro.local>
2022-06-30 12:54:41 +0200

    cleaning up a bit

--- test/accesscontrol.ts
+++ test/accesscontrol.ts
@@ -5,62 +5,53 @@ import { ethers } from "hardhat";
 export const testAccessControl = () => {
   describe("AccessControl", function () {
     let contract;
-    let donations: any;
+    let donationsContract: any;
     let owner: SignerWithAddress;
-    let addr1: SignerWithAddress;
-    let addr2: SignerWithAddress;
-    let addr3: SignerWithAddress;
+    let admin: SignerWithAddress;
+    let user: SignerWithAddress;
     const nullPointer =
       "0x0000000000000000000000000000000000000000000000000000000000000000";
-    let CAMPAIGN_CLOSED: string;
-    let CAMPAIGN_ACTIVE: string;
     beforeEach(async () => {
       contract = await ethers.getContractFactory("Donations");
-      [owner, addr1, addr2, addr3] = await ethers.getSigners();
+      [owner, admin, user] = await ethers.getSigners();
 
-      // To deploy our contract, we just have to call Token.deploy() and await
-      // for it to be deployed(), which happens once its transaction has been
-      // mined.
-      donations = await contract.deploy();
-      await donations.deployed();
-
-      CAMPAIGN_CLOSED = await donations.CAMPAIGN_CLOSED();
-      CAMPAIGN_ACTIVE = await donations.CAMPAIGN_ACTIVE();
+      donationsContract = await contract.deploy();
+      await donationsContract.deployed();
     });
 
     it("Should set to right owner", async () => {
-      const ownerOfContract = await donations._owner();
+      const ownerOfContract = await donationsContract._owner();
       expect(owner.address).to.be.equal(ownerOfContract);
     });
 
     it("Should check if adminstrator created and removed", async function () {
-      const adminTx = await donations.addAdminRole(addr1.address);
-      const adminRoleBytes = await donations.ADMIN_ROLE();
-      const getAdminStatus = await donations.checkIfAdministrator(
-        addr1.address
+      const adminTx = await donationsContract.addAdminRole(admin.address);
+      const adminRoleBytes = await donationsContract.ADMIN_ROLE();
+      const getAdminStatus = await donationsContract.checkIfAdministrator(
+        admin.address
       );
       expect(adminRoleBytes).to.be.equal(getAdminStatus);
 
       expect(adminTx)
-        .to.emit(donations, "AdminCreated")
-        .withArgs(addr1.address);
+        .to.emit(donationsContract, "AdminCreated")
+        .withArgs(admin.address);
 
-      const removeAdminTx = await donations.removeAdmin(addr1.address);
+      const removeAdminTx = await donationsContract.removeAdmin(admin.address);
       await removeAdminTx.wait();
 
       expect(adminTx)
-        .to.emit(donations, "AdminRemoved")
-        .withArgs(addr1.address);
+        .to.emit(donationsContract, "AdminRemoved")
+        .withArgs(admin.address);
 
-      const getRemovedAdmin = await donations.checkIfAdministrator(
-        addr1.address
+      const getRemovedAdmin = await donationsContract.checkIfAdministrator(
+        admin.address
       );
       expect(getRemovedAdmin).to.be.equal(nullPointer);
     });
 
     it("Should revert creating of admin", async () => {
       await expect(
-        donations.connect(addr2).addAdminRole(addr1.address)
+        donationsContract.connect(user).addAdminRole(admin.address)
       ).to.be.revertedWith("Only for owner");
     });
   });
--- test/campaigns.ts
+++ test/campaigns.ts
@@ -6,14 +6,12 @@ import { ethers } from "hardhat";
 export const testCampaign = () => {
   describe("Campaigns", function () {
     let contract;
-    let donations: any;
+    let donationsContract: any;
     let createDonationTx: any;
     let owner: SignerWithAddress;
-    let addr1: SignerWithAddress;
-    let addr2: SignerWithAddress;
-    let addr3: SignerWithAddress;
-    const nullPointer =
-      "0x0000000000000000000000000000000000000000000000000000000000000000";
+    let user1: SignerWithAddress;
+    let user2: SignerWithAddress;
+    let contractOwner: SignerWithAddress;
     let CAMPAIGN_CLOSED: string;
     let CAMPAIGN_ACTIVE: string;
     let expTime: any;
@@ -22,24 +20,21 @@ export const testCampaign = () => {
 
     beforeEach(async () => {
       contract = await ethers.getContractFactory("Donations");
-      [owner, addr1, addr2, addr3] = await ethers.getSigners();
+      [owner, user1, user2, contractOwner] = await ethers.getSigners();
 
-      // To deploy our contract, we just have to call Token.deploy() and await
-      // for it to be deployed(), which happens once its transaction has been
-      // mined.
-      donations = await contract.deploy();
-      await donations.deployed();
+      donationsContract = await contract.deploy();
+      await donationsContract.deployed();
 
-      CAMPAIGN_CLOSED = await donations.CAMPAIGN_CLOSED();
-      CAMPAIGN_ACTIVE = await donations.CAMPAIGN_ACTIVE();
+      CAMPAIGN_CLOSED = await donationsContract.CAMPAIGN_CLOSED();
+      CAMPAIGN_ACTIVE = await donationsContract.CAMPAIGN_ACTIVE();
 
-      await donations.addAdminRole(owner.address);
+      await donationsContract.addAdminRole(owner.address);
 
-      const blockTime = BigNumber.from(await donations.getBlockTime());
+      const blockTime = BigNumber.from(await donationsContract.getBlockTime());
       const oneEth = utils.parseEther("1.0");
       expTime = blockTime.add(10);
-      createDonationTx = await donations.createCampaign(
-        addr3.address,
+      createDonationTx = await donationsContract.createCampaign(
+        contractOwner.address,
         "test",
         "test",
         expTime,
@@ -49,10 +44,10 @@ export const testCampaign = () => {
 
     it("Should create campaign", async () => {
       await expect(createDonationTx)
-        .to.emit(donations, "CampaignCreated")
+        .to.emit(donationsContract, "CampaignCreated")
         .withArgs(
           owner.address,
-          addr3.address,
+          contractOwner.address,
           1,
           oneEth,
           CAMPAIGN_ACTIVE,
@@ -61,112 +56,120 @@ export const testCampaign = () => {
     });
 
     it("Should fail to create Campaign if user not Admin", async () => {
-      await donations.removeAdmin(owner.address);
+      await donationsContract.removeAdmin(owner.address);
       await expect(
-        donations.createCampaign(addr3.address, "test", "test", expTime, oneEth)
+        donationsContract.createCampaign(
+          contractOwner.address,
+          "test",
+          "test",
+          expTime,
+          oneEth
+        )
       ).to.be.revertedWith("Administrator role required");
     });
 
     it("Should close after founds are collected", async () => {
       let campaign;
-      const donationFirstTx = await donations
-        .connect(addr2)
+      const donationFirstTx = await donationsContract
+        .connect(user2)
         .donateToCampaign(campaignId, {
           value: utils.parseEther("0.3"),
         });
-      campaign = await donations.getCampaign(campaignId);
+      campaign = await donationsContract.getCampaign(campaignId);
       expect(campaign.status).to.be.equal(CAMPAIGN_ACTIVE);
       await expect(donationFirstTx)
-        .to.emit(donations, "DonationDeposited")
-        .withArgs(addr2.address, utils.parseEther("0.3"));
+        .to.emit(donationsContract, "DonationDeposited")
+        .withArgs(user2.address, utils.parseEther("0.3"));
 
-      const donationSecondTx = await donations
-        .connect(addr2)
+      const donationSecondTx = await donationsContract
+        .connect(user2)
         .donateToCampaign(campaignId, {
           value: utils.parseEther("0.2"),
         });
-      campaign = await donations.getCampaign(campaignId);
+      campaign = await donationsContract.getCampaign(campaignId);
       expect(campaign.status).to.be.equal(CAMPAIGN_ACTIVE);
       await expect(donationSecondTx)
-        .to.emit(donations, "DonationDeposited")
-        .withArgs(addr2.address, utils.parseEther("0.2"));
+        .to.emit(donationsContract, "DonationDeposited")
+        .withArgs(user2.address, utils.parseEther("0.2"));
 
-      const lastDonationTx = await donations
-        .connect(addr2)
+      const lastDonationTx = await donationsContract
+        .connect(user2)
         .donateToCampaign(campaignId, {
           value: utils.parseEther("0.6"),
         });
-      campaign = await donations.getCampaign(campaignId);
+      campaign = await donationsContract.getCampaign(campaignId);
       expect(campaign.status).to.be.equal(CAMPAIGN_CLOSED);
       await expect(lastDonationTx)
-        .to.emit(donations, "DonationDeposited")
-        .withArgs(addr2.address, utils.parseEther("0.5"));
+        .to.emit(donationsContract, "DonationDeposited")
+        .withArgs(user2.address, utils.parseEther("0.5"));
 
       await expect(lastDonationTx)
-        .to.emit(donations, "FundsCollected")
+        .to.emit(donationsContract, "FundsCollected")
         .withArgs(1, utils.parseEther("1.0"));
     });
 
     it("Should return exeeded amout after overpaid donation", async () => {
-      const balance = await addr2.getBalance();
-      const donationTx = await donations
-        .connect(addr2)
+      const balance = await user2.getBalance();
+      const donationTx = await donationsContract
+        .connect(user2)
         .donateToCampaign(campaignId, {
           value: utils.parseEther("2.0"),
         });
 
-      const balanceAfterDonation = await addr2.getBalance();
+      const balanceAfterDonation = await user2.getBalance();
       const spendedAmount = balance.sub(balanceAfterDonation);
       await expect(donationTx)
-        .to.emit(donations, "DonationAmountReturned")
-        .withArgs(addr2.address, utils.parseEther("1.0"));
+        .to.emit(donationsContract, "DonationAmountReturned")
+        .withArgs(user2.address, utils.parseEther("1.0"));
       expect(spendedAmount)
         .to.be.above(utils.parseEther("1.0"))
         .and.to.be.below(utils.parseEther("1.001"));
     });
 
     it("Should expire after some time", async () => {
-      await donations.connect(addr2).donateToCampaign(campaignId, {
+      await donationsContract.connect(user2).donateToCampaign(campaignId, {
         value: utils.parseEther("0.1"),
       });
 
       await new Promise((resolve) => setTimeout(resolve, 12000));
       await expect(
-        donations.connect(addr2).donateToCampaign(campaignId, {
+        donationsContract.connect(user2).donateToCampaign(campaignId, {
           value: utils.parseEther("0.1"),
         })
       ).to.be.revertedWith("campaign has ended");
     }).timeout(20000);
 
     it("Should withdrawal funds from campaign", async () => {
-      await donations.connect(addr2).donateToCampaign(campaignId, {
+      await donationsContract.connect(user2).donateToCampaign(campaignId, {
         value: utils.parseEther("2"),
       });
 
-      const contractBallance = await donations.getBalance();
+      const contractBallance = await donationsContract.getBalance();
       expect(contractBallance).to.be.equal(utils.parseEther("1.0"));
-      const campaignWalletBalance = await addr3.getBalance();
+      const campaignWalletBalance = await contractOwner.getBalance();
 
-      const wdTx = await donations.connect(addr3).withdrawal(campaignId);
-      const campaignWalletBalanceAfter = await addr3.getBalance();
+      const wdTx = await donationsContract
+        .connect(contractOwner)
+        .withdrawal(campaignId);
+      const campaignWalletBalanceAfter = await contractOwner.getBalance();
       expect(campaignWalletBalance).to.be.below(campaignWalletBalanceAfter);
       await expect(wdTx)
-        .to.emit(donations, "FundsWithdrawed")
-        .withArgs(addr3.address, utils.parseEther("1.0"));
+        .to.emit(donationsContract, "FundsWithdrawed")
+        .withArgs(contractOwner.address, utils.parseEther("1.0"));
     });
 
     it("Should fail to withdrawal if campaign not over", async () => {
       await expect(
-        donations.connect(addr3).withdrawal(campaignId)
+        donationsContract.connect(contractOwner).withdrawal(campaignId)
       ).to.be.revertedWith("Campaign is still active");
     });
 
     it("Should fail to withdrawal user is not owner of campaign", async () => {
-      await donations.connect(addr2).donateToCampaign(campaignId, {
+      await donationsContract.connect(user2).donateToCampaign(campaignId, {
         value: utils.parseEther("2"),
       });
       await expect(
-        donations.connect(addr1).withdrawal(campaignId)
+        donationsContract.connect(user1).withdrawal(campaignId)
       ).to.be.revertedWith("No access to withdrawal");
     });
   });
